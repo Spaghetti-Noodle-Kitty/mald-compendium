cb83d53e41361800a340d1f8f36493f1486ca080
Zoe <elias.m.decorvet@gmail.com>
2023-01-14 23:43:44 +0100

    Added basis of math-class
    I feel so dumb for skipping classes :(

---
+++ zMath.cpp
@@ -0,0 +1,221 @@
+#include "zMath.h"
+
+#pragma region Static Data
+double Z_PI		= 3.14159265358979;
+double Z_Euler	= 2.71828182845904;
+double Deg2Rad = 360 / (Z_PI * 2);
+double Rad2Deg	= (Z_PI * 2) / 360;
+
+#pragma endregion
+
+#pragma region Datatypes
+class Vector2D
+{
+public:
+	double X, Y, R, F;
+	Vector2D(double PosX, double PosY, double Rot, double Force)
+	{
+		X = PosX;
+		Y = PosY;
+		R = Clamp(0, 360, Rot);
+		F = Force;
+	}
+};
+
+class Vector3D
+{
+public:
+	double X, Y, Z, Rx, Ry, Rz, F;
+	Vector3D(double PosX, double PosY, double PosZ, double RotX, double RotY, double RotZ, double Force)
+	{
+		X = PosX;
+		Y = PosY;
+		Z = PosZ;
+		Rx = Clamp(0, 360, RotX);
+		Ry = Clamp(0, 360, RotY);
+		Rz = Clamp(0, 360, RotZ);
+		F = Force;
+	}
+};
+#pragma endregion
+
+#pragma region Functions
+double zMath::Clamp(double Min, double Max, double Num)
+{
+	if (Num < Min)
+		return Min;
+	else if (Num > Max)
+		return Max;
+	else
+		return Num;
+}
+
+double zMath::Sin(double AngleRad)
+{
+	double res = AngleRad;
+	double term = AngleRad;
+	int i = 3;
+	while (i < 15)
+	{
+		term = -term * AngleRad * AngleRad / (i * (i - 1));
+		res += term;
+		i += 2;
+	}
+	return res;
+}
+
+double zMath::Cos(double AngleRad)
+{
+	double res = AngleRad;
+	double term = AngleRad;
+	int i = 2;
+	while (i < 14)
+	{
+		term = -term * AngleRad * AngleRad / (i * (i - 1));
+		res += term;
+		i += 2;
+	}
+	return res;
+}
+
+double zMath::Tan(double AngleRad)
+{
+	return Sin(AngleRad) / Cos(AngleRad);
+}
+
+double zMath::Sqrt(double Num)
+{
+	double l = 0;
+	double r = Num;
+	double p = 1e-10;
+	double m = 0;
+
+	while (r - l > p)
+	{
+		m = (l + r) / 2;
+		if (m * m < Num)
+			l = m;
+		else
+			r = m;
+	}
+	return (l + r) / 2;
+}
+
+double zMath::Sign(double Num)
+{
+	if (Num > 0)
+		return 1;
+	else
+		return 0;
+}
+
+double zMath::Pow(double Num, double Exp)
+{
+	double res = 1;
+	for (int i = 0; i < Exp; i++)
+		res *= Num;
+	return res;
+}
+
+double zMath::Min(double Nums[])
+{
+	double res = Nums[0];
+	for (int i = 1; i < (sizeof(Nums) / sizeof(Nums[0])); i++)
+	{
+		if (Nums[i] < res)
+			res = Nums[i];
+	}
+	return res;
+}
+
+double zMath::Max(double Nums[])
+{
+	double res = Nums[0];
+	for (int i = 1; i < (sizeof(Nums) / sizeof(Nums[0])); i++)
+	{
+		if (Nums[i] > res)
+			res = Nums[i];
+	}
+	return res;
+}
+
+double zMath::Log(double Num)
+{
+	double res = 0;
+	double term = Num - 1;
+	double k = 1;
+	while (term > 1e-10)
+	{
+		term = term * (Num - 1) / Num;
+		res += term / k;
+		k++;
+	}
+	return res;
+}
+
+double zMath::Log10(double Num)
+{
+	return Log(Num) / Log(10);
+}
+
+double zMath::Exp(double Num)
+{
+	return Pow(Z_Euler, Num);
+}
+
+double zMath::Atan2(double y, double x)
+{
+	// https://github.com/DiamonDinoia my beloved <3 //
+
+	double a, r, s, t, c, q, ax, ay, mx, mn;
+	ax = x < 0 ? -x : x;
+	ay = y < 0 ? -y : y;
+
+	double ArrA[] = { ay, ax };
+
+	mx = Max(ArrA);
+	mn = Min(ArrA);
+	a = mn / mx;
+	s = a * a;
+	c = s * a;
+	q = s * s;
+	r = 0.024840285f * q + 0.18681418f;
+	t = -0.094097948f * q - 0.33213072f;
+	r = r * s + t;
+	r = r * c + a;
+	if (ay > ax) r = 1.57079637f - r;
+	if (x < 0) r = 3.14159274f - r;
+	if (y < 0) r = -r;
+	return r;
+}
+
+double zMath::Atan(double AngleRad)
+{
+	double res = 0;
+	for (int n = 0; n < 15; n++)
+		res += Pow(-1, n) * Pow(AngleRad, 2 * n + 1) / (2 * n + 1);
+	return res;
+}
+
+double zMath::Asin(double AngleRad)
+{
+	return Z_PI / 2 - Acos(AngleRad);
+}
+
+double zMath::Acos(double AngleRad)
+{
+	double Y = Sqrt(1 - AngleRad * AngleRad);
+	return Atan2(Y, AngleRad);
+}
+
+double zMath::Floor(double Num)
+{
+	return Num < 0 ? (int)(Num - 0.5) : (int)(Num);
+}
+
+double zMath::Ceil(double Num)
+{
+	return Num < 0 ? (int)(Num) : (int)(Num + 0.5);
+}
+#pragma endregion
+
---
+++ zMath.h
@@ -0,0 +1,131 @@
+#pragma once
+class zMath
+{
+public:
+	Vector2D Vector2D;
+	Vector3D Vector3D;
+
+	// Static values //
+	static double Z_PI;
+	static double Z_Euler;
+	static double Deg2Rad;
+	static double Rad2Deg;
+
+	// Functions //
+	/// <summary>
+	/// Clamps the Num-Value to be between Min&Max,
+	/// if Num is smaller than Min, Min will be returned
+	/// if Num is larger than Max, Max will be returned
+	/// otherwise Num will be returned
+	/// </summary>
+	/// <param name="Min">Minimum value</param>
+	/// <param name="Max">Maximum value</param>
+	/// <param name="Num">Clamp value</param>
+	/// <returns></returns>
+	static double Clamp	(double Min, double Max, double Num);
+	/// <summary>
+	/// Gets the sine of an angle represented in radians
+	/// </summary>
+	/// <param name="AngleRad">The angle in radians</param>
+	/// <returns></returns>
+	static double Sin	(double AngleRad);
+	/// <summary>
+	/// Gets the cosine of an angle represented in radians
+	/// </summary>
+	/// <param name="AngleRad">The angle in radians</param>
+	/// <returns></returns>
+	static double Cos	(double AngleRad);
+	/// <summary>
+	/// Gets the tangent of an angle in radians
+	/// </summary>
+	/// <param name="AngleRad">The angle in radians</param>
+	/// <returns></returns>
+	static double Tan	(double AngleRad);
+	/// <summary>
+	/// Gets the arc-sine of an angle represented in radians
+	/// </summary>
+	/// <param name="AngleRad">The angle in radians</param>
+	/// <returns></returns>
+	static double Asin	(double AngleRad);
+	/// <summary>
+	/// Gets the arc-cosine of the angle represented in radians
+	/// </summary>
+	/// <param name="AngleRad">The angle in radians</param>
+	/// <returns></returns>
+	static double Acos	(double AngleRad);
+	/// <summary>
+	/// Gets the arc-tangent of the angle represented in radians
+	/// </summary>
+	/// <param name="AngleRad">The angle in radians</param>
+	/// <returns></returns>
+	static double Atan	(double AngleRad);
+	/// <summary>
+	/// Gets the value of Atan2() in radians
+	/// </summary>
+	/// <param name="y">Y</param>
+	/// <param name="x">X</param>
+	/// <returns></returns>
+	static double Atan2	(double y, double x);
+	/// <summary>
+	/// Returns the square-root of the passed Number via approximation
+	/// </summary>
+	/// <param name="Num">Number to get Square-root</param>
+	/// <returns></returns>
+	static double Sqrt	(double Num);
+	/// <summary>
+	/// Returns 1 if the number is positive
+	/// Returns 0 if the number is negative
+	/// </summary>
+	/// <param name="Num">The number to be checked</param>
+	/// <returns></returns>
+	static double Sign	(double Num);
+	/// <summary>
+	/// Returns Num raised to the power of Exp
+	/// </summary>
+	/// <param name="Num"></param>
+	/// <param name="Exp"></param>
+	/// <returns></returns>
+	static double Pow	(double Num, double Exp);
+	/// <summary>
+	/// Return the lowest value in the Nums[] array
+	/// </summary>
+	/// <param name="Nums">Array of doubles</param>
+	/// <returns></returns>
+	static double Min	(double Nums[]);
+	/// <summary>
+	/// Returns the highest number in the Nums[] array
+	/// </summary>
+	/// <param name="Nums">Array of doubles</param>
+	/// <returns></returns>
+	static double Max	(double Nums[]);
+	/// <summary>
+	/// Returns the logarithm of the specified number
+	/// </summary>
+	/// <param name="Num">A number</param>
+	/// <returns></returns>
+	static double Log	(double Num);
+	/// <summary>
+	/// Returns the Base-10 logarithm of the passed number
+	/// </summary>
+	/// <param name="Num">A number</param>
+	/// <returns></returns>
+	static double Log10	(double Num);
+	/// <summary>
+	/// Returns Euler's raised to the power of passed number
+	/// </summary>
+	/// <param name="Num">A number</param>
+	/// <returns></returns>
+	static double Exp	(double Num);
+	/// <summary>
+	/// Floors the number to nearest integer
+	/// </summary>
+	/// <param name="Num">A double</param>
+	/// <returns></returns>
+	static double Floor	(double Num);
+	/// <summary>
+	/// Ceils the number to nearest integer
+	/// </summary>
+	/// <param name="Num">A double</param>
+	/// <returns></returns>
+	static double Ceil	(double Num);
+};
